d062f041e7ab5805676a3da47a9301dadc28a33f
Pontus <Pontus_thoren@hotmail.com>
2025-11-10 09:36:15 +0100

    Added a bakground pitcure

---
+++ build-apk.bat
@@ -0,0 +1,12 @@
+﻿@echo off
+REM ----------------------------
+REM Bygg APK för Android
+REM ----------------------------
+dotnet publish -f:net8.0-android -c:Release -o:publish/Android
+
+echo.
+echo ===============================
+echo APK byggd!
+echo Filen ligger i: publish/Android/
+echo ===============================
+pause
